c5221b4a5a4ff6032b264ef9465645993ea7db6f
Ismael <izigelbaum@gmail.com>
2013-04-08 03:28:51 +0200

    new changes

--- js/collections/cartCollection.js
+++ js/collections/cartCollection.js
@@ -5,15 +5,7 @@ define(['backbone', 'localStorage', 'models/cartModel', 'views/cartView'],
         div: "#cart-info",
         model: Cart,
 
-        localStorage: new Store('CartCollection'),//Backbone.LocalStorage('CartCollection'),
-
-        initialize: function(models, options){
-            this.listenTo(this, 'change', this.localS());
-        },
-
-        localS: function(){
-            console.log('cambiado');
-        },
+        localStorage: new Store('CartCollection'),
 
         filterById: function(id){
             for( i = 0 ; i < this.length; i++ ){
